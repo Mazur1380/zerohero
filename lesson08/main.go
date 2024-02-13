5a39909fc9e8fb4e5a7caf98a5413973c5f3eb0a
Mazur <vlad882008@mail.ru>
2024-02-13 21:07:39 +0300

    end lesson08

---
+++ lesson08/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var age int
+	// age = 18
+
+	// var age int = 18
+	// var age = 18
+	age := 16
+	if age >= 18 {
+		fmt.Println("Тебе можно в кальянную")
+
+	} else {
+		fmt.Println("Тебе нельзя в кальянную")
+
+	}
+
+	// if age < 18 {
+	// 	fmt.Println("Тебе нельзя в кальянную")
+	// }
+
+}
