c5cf11c7a59353f6f1706a11eac3d6ec25a5fa33
Mazur <vlad882008@mail.ru>
2024-02-27 19:50:05 +0300

    end lesson39

---
+++ lesson39/main.go
@@ -0,0 +1,24 @@
+package main
+
+func main() {
+
+	//for x := 5; x < 15; x++ {
+	//	fmt.Println("hello world", x)
+	//}
+	//for x := 0; x < 11; x = x + 2 {
+	//	fmt.Println(x)
+	//}
+
+	x := []string{"a", "b", "c", "d", "e"}
+	//for i := 0; i < len(x); i++ {
+	//	fmt.Println(x[i])
+	//	}
+
+	//	for i := range x {
+	//	fmt.Println(x[i])
+	//}
+	//	for i, e := range x {
+	//	fmt.Println(i, e)
+	//}
+
+}
