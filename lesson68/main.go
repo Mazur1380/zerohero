96e5b227e1d4ccfdedc38b98a010fd5a793d8261
Mazur <vlad882008@mail.ru>
2024-03-09 15:21:50 +0300

    end lesson

---
+++ lesson68/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var x string
+	fmt.Println(x)
+	var y bool
+	fmt.Println(y)
+	var b [5]float64
+	fmt.Println(b)
+	var c []int
+	fmt.Println(c)
+	var k map[int]string
+	fmt.Println(k)
+	var fuck struct {
+		name string
+	}
+	fmt.Println(fuck)
+}
