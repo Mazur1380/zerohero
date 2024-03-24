ccb04d1b844a825d8d7c79e3f58fbff2d535a691
Mazur <vlad882008@mail.ru>
2024-03-24 19:45:12 +0300

    end comand sort

--- lesson85/main.go
+++ lesson85/main.go
@@ -3,20 +3,25 @@ package main
 import (
 	"fmt"
 	"math"
+	"sort"
 )
 
 func main() {
 	x := []int{9, 3, 5, 4, 7, 3, 7, 5, 3, 8, 5}
 
-	sort(x)
+	Sort(x)
 	fmt.Println(x)
+	y := []int{9, 3, 5, 4, 7, 3, 7, 5, 3, 8, 5}
+	SortSTD(y)
+	fmt.Println(y)
+
 }
 func Swop(x []int) {
-	min := math.MaxInt
+	max := math.MinInt
 	index := -1
 	for i := 0; i < len(x); i++ {
-		if x[i] < min {
-			min = x[i]
+		if x[i] > max {
+			max = x[i]
 			index = i
 		}
 
@@ -24,9 +29,13 @@ func Swop(x []int) {
 	x[0], x[index] = x[index], x[0]
 }
 
-func sort(x []int) {
+func Sort(x []int) {
 	for i := 0; i < len(x)-1; i++ {
 		Swop(x[i:])
 	}
-
+}
+func SortSTD(y []int) {
+	sort.Slice(y, func(i int, j int) bool {
+		return y[i] > y[j]
+	})
 }
