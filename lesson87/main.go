fea2d8a79cb2af6ab1189b3c9713a8def44a6d66
Mazur <vlad882008@mail.ru>
2024-03-25 17:51:42 +0300

    end lesson

--- lesson87/main.go
+++ lesson87/main.go
@@ -3,21 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	t := []int{5, 3, 9, 4, 8, 2}
-	n := Exist(t, 1)
-	if n {
-		fmt.Println("Done")
+	t := []int{5, 3, 9, 4, 4, 4}
+	n := Exist(t, 4)
+	if n > 0 {
+		fmt.Println("Done", n)
 	} else {
 		fmt.Println("Not found")
 	}
 
 }
 
-func Exist(x []int, y int) bool {
+func Exist(x []int, y int) int {
+	counter := 0
 	for i := range x {
 		if x[i] == y {
-			return true
+			counter = counter + 1
 		}
 	}
-	return false
+	return counter
+
 }
