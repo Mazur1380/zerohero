0b6a3412c15624d1aa4ebcf186b886e6360361b9
Mazur <vlad882008@mail.ru>
2024-06-08 19:20:50 +0300

    end lesson

--- lesson136/main.go
+++ lesson136/main.go
@@ -11,17 +11,23 @@ func main() {
 		{name: "Vlad", balance: 200},
 		{name: "Djas", balance: 400},
 		{name: "Dima", balance: 600},
+		{name: "Alex", balance: 600},
 	}
 	fmt.Println(us)
 
 	max := math.MinInt
+	maxUsers := []User{}
 
 	for _, u := range us {
 		if u.balance > max {
 			max = u.balance
+			maxUsers = []User{}
+			maxUsers = append(maxUsers, u)
+		} else if u.balance == max {
+			maxUsers = append(maxUsers, u)
 		}
 	}
-	fmt.Println(max)
+	fmt.Println(maxUsers)
 }
 
 type User struct {
